074f6766ecff2faf761a5feb709b500a7c29fe04
WooYoo <puba5@naver.com>
2020-07-04 05:20:02 +0900

    feat : 동영상 자동 재생
    크롬에서는 muted 옵션을 넣어야 동영상 자동 재생이 됨

--- pages/splash.tsx
+++ pages/splash.tsx
@@ -5,7 +5,7 @@ export default function Splash() {
   return (
     <Wrapper>
       <MainHeader />
-      <video id="background-video" width="100%" height="100%" loop autoPlay>
+      <video id="background-video" width="100%" height="100%" loop autoPlay muted>
         <source
           src={
             "https://firebasestorage.googleapis.com/v0/b/esque-66147.appspot.com/o/esquevideo%2F%EC%98%81%EC%83%81%20%EC%98%88%EC%8B%9C1.mp4?alt=media&token=a583cc80-b418-455e-aa51-4b16102289d1"
