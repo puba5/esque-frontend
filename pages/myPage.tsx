ef425507fb1c795f97bd2d42feb6ddec14a800f8
WooYoo <puba5@naver.com>
2020-07-17 01:45:36 +0900

    feat : My product 버튼 추가
    my product 페이지에 버튼 추가

--- pages/myPage.tsx
+++ pages/myPage.tsx
@@ -8,7 +8,12 @@ export default function myPage() {
   return (
     <Wrapper>
       <MyHeader />
-      <ProductControll></ProductControll>
+      <ProductControll>
+        <ControllButton>
+          <SelectText>전체선택(2/3)</SelectText>
+          <DeleteButton>삭제</DeleteButton>
+        </ControllButton>
+      </ProductControll>
       <MyProduct></MyProduct>
       <MyProduct></MyProduct>
       <MyProduct></MyProduct>
@@ -25,5 +30,39 @@ const Wrapper = styled.div`
 const ProductControll = styled.div`
   width: 100%;
   height: 10.8rem;
+  padding-top: 6.8rem;
   background: #f4f4f4;
+`;
+const SelectButton = styled.div``;
+const SelectText = styled.a`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.4rem;
+  line-height: 2.1rem;
+  letter-spacing: -0.02em;
+  color: #2b2b2b;
+`;
+const ControllButton = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid red;
+  margin: 0 auto;
+  height: 3.6rem;
+  width: 34.8rem;
+`;
+
+const DeleteButton = styled.button`
+  margin-left: auto;
+  border: none;
+  width: 6.1rem;
+  height: 2.6rem;
+  background: #ffffff;
+  border-radius: 20px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.4rem;
+  line-height: 2.1rem;
+  text-align: center;
+  letter-spacing: -0.02em;
+  color: #2b2b2b;
 `;
